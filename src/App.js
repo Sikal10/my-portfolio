99919ea0018a8722f4d9e600fd49452b3c437361
abiolasikal <sikal.sikal.ss@gmail.com>
2022-08-14 21:55:19 +0100

    favicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import {ThemeProvider} from "styled-components";
+import {lightTheme} from "./styles/ThemeProvider";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Skills from "./pages/Skills/Skills";
+import Works from "./pages/Works/Works";
+import {AnimatePresence} from "framer-motion";
+
 function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <GlobalStyles/>
+
+            <AnimatePresence>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/about"} element={<About/>}/>
+                    <Route path={"/contact"} element={<Contact/>}/>
+                    <Route path={"/skills"} element={<Skills/>}/>
+                    <Route path={"/projects"} element={<Works/>}/>
+                </Routes>
+            </AnimatePresence>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,10 @@
 import {createGlobalStyle} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    
+  
+  //import google font
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+  
     *, h1,h2,h3,h4,h5,h6 {
       margin: 0;
       padding: 0;
@@ -21,6 +24,7 @@ const GlobalStyle = createGlobalStyle`
       margin: 0;
       padding: 0;
       overflow-x: hidden;
+      font-family: 'Lato', sans-serif;
 
       ::-webkit-scrollbar {
         display: none;
